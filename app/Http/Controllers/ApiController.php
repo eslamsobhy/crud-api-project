94be235cd53f60b323b5913fd7c7c0e12a95ffd5
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-01-17 17:29:01 +0200

    setup the controller methods

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ApiController extends Controller
+{
+    public function getAllStudents(){
+        // logic
+    }
+
+    public function createStudent(Request $request){
+        // logic
+    }   
+
+    public function getStudent($id){
+        // logic
+    }
+
+    public function updateStudent(Request $request, $id){
+        // logic
+    }
+
+    public function deleteStudent($id){
+        // logic
+    }
+}
